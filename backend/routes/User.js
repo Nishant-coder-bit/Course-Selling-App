86338a6efee1e89202e84519554a6d54b7da0d07
Nishant Raj <nishantanindian@gmail.com>
2024-09-29 03:17:34 +0530

    bcryt added

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User= require("../models/User");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");    
+ const bcrypt = require("bcrypt");
+ const jwt = require("jsonwebtoken");    
 
 router.use(express.json());
 
